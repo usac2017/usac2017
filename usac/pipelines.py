f06c5cb81e45f670ae54b96d94b0e9586f603dff
anisim <89anisim89@gmail.com>
2017-10-25 10:54:16 +1000

    fixed both sources

--- usac/pipelines.py
+++ usac/pipelines.py
@@ -4,35 +4,45 @@ import datetime,re,requests
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-REPORT_DAYS = 7
+REPORT_DAYS = 1
 MAILGUN_KEY = 'key-297e3d8d51b08f45259c1f9d9191548d'
 MAILGUN_SANDBOX = 'sandboxbb254b3e33c14969979996793b5e3ec2.mailgun.org'
 MAILGUN_RECIPIENT = 'daniel@erateconsulting.org'
 
-def sendHTML(html):
+def today():
+  return datetime.datetime.now().strftime('%Y-%m-%d')
+
+def sendEMAIL(html='',csv='',total=0):
   request_url = 'https://api.mailgun.net/v2/{0}/messages'.format(MAILGUN_SANDBOX)
-  request = requests.post(request_url, auth=('api', MAILGUN_KEY), data={
-    'from': 'usac@scrappinghub.com',
+  data={
+    'from': 'FRN Denial Job <usac@scrappinghub.com>',
     'to': MAILGUN_RECIPIENT,
-    'subject': 'Hello',
-    'text': 'Your mail do not support HTML',
-    'html': html
-  })  
-
+    'subject': 'UASAC FRNs Denied on %s' % datetime.datetime.now().strftime('%Y-%m-%d'),
+    'text': 'Founded %s %s in %s %s.' % (total,'item' if total==1 else 'items',REPORT_DAYS,'day' if REPORT_DAYS==1 else 'days')
+  }
+  if html:
+    data['html'] = html
+  files = [("attachment", ("report%s.csv" % today(), csv))] if csv and total else []
+  request = requests.post(request_url, auth=('api', MAILGUN_KEY), data=data,files=files)
+  
 reportFields = [
-    'FCDLDate', 'FourSeventyOneApplicationStatus', 'BilledEntityName', 'BEN', 'ApplicantState',
-    'ApplicantType', 'FourSeventyOneContactName', 'FourSeventyOneContactEmail', 'BENAccountAdministrator',
-    'BENAccountAdministratorEmail', 'FourSeventyOneConsultingFirmName', 'FRN', 'FRNNickname', 'FRNStatus',
-    'ServiceProviderName', 'OrigFundingRequest', 'CmtdFundingRequest', 'OrigDiscount', 'OrigFRNServiceType',
-    'FourSeventyOneApplicationNumber', 'FourSeventyOneNickName'
+    'FCDLDate', 'FRNStatus', 'BilledEntityName', 'BEN', 'ApplicantState',
+    'FourSeventyOneContactEmail', 'ApplicantType', 'FCDLCommentforFRN', 'FCDLCommentforFourSeventyOneApplication',
+    'FourSeventyOneContactName', 'BENAccountAdministrator', 'BENAccountAdministratorEmail', 'FourSeventyOneConsultingFirmName',
+    'OrigFundingRequest', 'CmtdFundingRequest', 'OrigDiscount', 'OrigFRNServiceType',
+    'FRN', 'FourSeventyOneApplicationNumber'
   ]
-
+  
 def makeHTMLTR(item):
   tr = '<tr>'
   for field in reportFields:
     tr+= '<td>'+str(item[field])+'</td>'
   tr+= '</tr>'  
   return tr
+
+def makeCSVrow(item):
+  row = ','.join(['"'+str(item[field]).replace('"',"'")+'"' for field in reportFields])+'\n'
+  return row
   
 def makeHTMLReport(TRs):
   html = '<html><body>'
@@ -50,6 +60,12 @@ def makeHTMLReport(TRs):
   html+= '</body></html>'
   return html  
 
+def makeCSVReport(csv_rows):
+  csv = ','.join(['"%s"' % f for f in reportFields])+'\n'
+  for row in csv_rows:
+    csv+= row
+  return csv
+  
 def isPresent(raw,present_date):
   if not re.match('\d{2}/\d{2}/\d{4}',str(raw)):
     return False 
@@ -61,12 +77,15 @@ class UsacPipeline(object):
     def process_item(self, item, spider):
         if isPresent(item['FCDLDate'],self.present_date):
           self.TRs.append(makeHTMLTR(item))
+          self.csv_rows.append(makeCSVrow(item))
         return item
     
     def open_spider(self,spider):
         self.present_date = datetime.datetime.now()-datetime.timedelta(days=REPORT_DAYS+1)
         self.TRs = []
+        self.csv_rows = []
     
     def close_spider(self,spider):
         html = makeHTMLReport(self.TRs)
-        sendHTML(html)
+        csv = makeCSVReport(self.csv_rows)
+        sendEMAIL(csv=csv,total=len(self.csv_rows))
